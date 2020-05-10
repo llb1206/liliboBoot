3bb086f97a252a0d122bff97b9f9b1812e66f100
1206llb <896427277@qq.com>
2020-05-10 20:38:03 +0800

    first

---
+++ bootx1/src/main/java/com/redis/domain/UserVo.java
@@ -0,0 +1,13 @@
+package com.redis.domain;
+
+import lombok.Data;
+
+@Data
+public class UserVo {
+
+    public  static final String Table = "t_user";
+
+    private String name;
+    private String address;
+    private Integer age;
+}
