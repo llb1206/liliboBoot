2a66131931429b2308f0a8e1c3eb0da40212ec24
llb1206 <896427277@qq.com>
2020-05-22 10:56:11 +0800

    啧啧啧

--- bootx1/src/main/java/com/filter/controller/SsController.java
+++ bootx1/src/main/java/com/filter/controller/SsController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpSession;
 
 @RestController
 public class SsController {
-
+    //http://localhost:1111/login?name=root&pwd=root
     @RequestMapping("login")
     public String login(String name, String pwd, HttpServletRequest request) {
         HttpSession session = request.getSession();
@@ -23,19 +23,20 @@ public class SsController {
         }
     }
 
-    @RequestMapping("ss")
-    public String logins(String name, String pwd, HttpServletRequest request) {
-        HttpSession session = request.getSession();
-
-        if (name.equals("root") && pwd.equals("root")) {
-            User user = new User();
-            user.setName(name);
-            session.setAttribute("user", user);
-            return "登录成功";
-        } else {
-            return "用户名或密码错误!";
-        }
+    //http://localhost:1111/test
+    @RequestMapping("test")
+    public String logins() {
+        return "访问suc-有权限";
     }
+
+    //http://localhost:1111/ss
+    @RequestMapping("zx")
+    public void zhuxiao(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        session.removeAttribute("user");
+    }
+
 
     public class User {
         private String name;
--- bootx1/src/main/java/com/filter/filter/SessionFilter.java
+++ bootx1/src/main/java/com/filter/filter/SessionFilter.java
@@ -6,12 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-/**
- * @Author:Lvxingqing
- * @Description:
- * @Date:Create in 10:57 2018/3/12
- * @Modified By:
- */
+
 //@WebFilter(filterName = "sessionFilter",urlPatterns = {"/*"})
 public class SessionFilter implements Filter {
 
@@ -19,7 +14,7 @@ public class SessionFilter implements Filter {
     String NO_LOGIN = "您还未登录";
 
     //不需要登录就可以访问的路径(比如:注册登录等)
-    String[] includeUrls = new String[]{"/login","/register","/user/login.html"};
+    String[] includeUrls = new String[]{"/login", "/register", "/user/login.html"};
 
 
     @Override
@@ -29,42 +24,26 @@ public class SessionFilter implements Filter {
         HttpSession session = request.getSession(false);
         String uri = request.getRequestURI();
 
-        System.out.println("filter url:"+uri);
+        System.out.println("filter url:" + uri);
         //是否需要过滤
         boolean needFilter = isNeedFilter(uri);
 
-
-        if (!needFilter) { //不需要过滤直接传给下一个过滤器
+        //不需要过滤直接传给下一个过滤器
+        if (!needFilter) {
             filterChain.doFilter(servletRequest, servletResponse);
         } else { //需要过滤器
-            // session中包含user对象,则是登录状态
-          if(session!=null&&session.getAttribute("user") != null){
-                // System.out.println("user:"+session.getAttribute("user"));
+            if (session != null && session.getAttribute("user") != null) {
                 filterChain.doFilter(request, response);
-            }else{
-                String requestType = request.getHeader("X-Requested-With");
-                //判断是否是ajax请求
-                if(requestType!=null && "XMLHttpRequest".equals(requestType)){
-                    response.getWriter().write(this.NO_LOGIN);
-                }else{
-                    //重定向到登录页(需要在static文件夹下建立此html文件)
-                    response.sendRedirect(request.getContextPath()+"/user/login.html");
-                }
-                return;
+            } else {
+                throw new SecurityException("无访问权限");
             }
         }
     }
 
-    /**
-     * @Author: Lvxianqing
-     * @Description: 是否需要过滤
-     * @Date: 2018-03-12 13:20:54
-     * @param uri
-     */
     public boolean isNeedFilter(String uri) {
 
         for (String includeUrl : includeUrls) {
-            if(includeUrl.equals(uri)) {
+            if (includeUrl.equals(uri)) {
                 return false;
             }
         }
