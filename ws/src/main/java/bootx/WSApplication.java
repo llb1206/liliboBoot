6d27d37b55e5f8dca421aa712dd1ba3306fce406
1206llb <896427277@qq.com>
2020-05-30 17:35:53 +0800

    first

---
+++ ws/src/main/java/bootx/WSApplication.java
@@ -0,0 +1,13 @@
+package bootx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WSApplication.class, args);
+    }
+
+}
