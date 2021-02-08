a9bfdf1c1abc3e9c3a0eace8f2104bdc8a2d6d96
1500058742@qq.com <1206c3011>
2021-02-08 12:33:24 +0800

    dasdasdsad

--- bootx1/src/main/java/com/dataStru/test/tree/sort/MergeSort.java
+++ bootx1/src/main/java/com/dataStru/test/tree/sort/MergeSort.java
@@ -2,14 +2,24 @@ package com.dataStru.test.tree.sort;
 
 import java.util.Arrays;
 
+/**
+ * @author 15000
+ * 动画地址  https://cloud.tencent.com/developer/article/1377640
+ */
 public class MergeSort {
 
     public static void merge(int[] arr, int low, int mid, int high) {
         int[] temp = new int[high - low + 1];
-        int i = low;// 左指针
-        int j = mid + 1;// 右指针
+
+        /**
+         * 把数组分成了两半  第一半从low 开始 第二半动mid+1开始
+         */
+        // 左段起始指针
+        int i = low;
+        // 右段起始指针
+        int j = mid + 1;
         int k = 0;
-        // 把较小的数先移到新数组中
+        // 把较小的数先移到新数组中  看动画理解  地址上面
         while (i <= mid && j <= high) {
             if (arr[i] < arr[j]) {
                 temp[k++] = arr[i++];
@@ -31,14 +41,20 @@ public class MergeSort {
         }
     }
 
+    /**
+     * 每次传一段  然后段段切割 轮回不止
+     */
     public static void mergeSort(int[] arr, int low, int high) {
         int mid = (low + high) / 2;
         if (low < high) {
-            // 左边
+            //  第一阶段  左右切割  并没有真正的切割数组  也是数组分段
+            // 左边  将数组切割成段  先左后右
             mergeSort(arr, low, mid);
             // 右边
             mergeSort(arr, mid + 1, high);
-            // 左右归并
+            // 第二阶段  开始合并
+            // 左右归并 排序融合
+            // 这是最小的那一段
             merge(arr, low, mid, high);
             System.out.println(Arrays.toString(arr));
         }
